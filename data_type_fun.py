fafbdde35a06de199a7416de4901cf13c78ccbe6
donotkg <donovan.takang@gmail.com>
2025-11-21 15:54:31 +0100

    Update data_type_fun.py

--- data_type_fun.py
+++ data_type_fun.py
@@ -1,12 +1,15 @@
-number = "42"
+number = "42"               # Store the string "42" in the variable 'number'
 
-print(number)
+print(number)               # Print the string value "42"
 
-print(type(number))
+print(type(number))         # Display the data type of 'number' (it will be <class 'str'>)
 
-int_number = int(number)
+int_number = int(number)    # Convert the string "42" into an integer and store it in 'int_number'
 
-print(int_number)
+print(int_number)           # Print the integer value 42
 
-print(type(int_number))
-print(number + "5")
+print(type(int_number))     # Display the data type of 'int_number' (it will be <class 'int'>)
+
+print(int_number + 5)       # Integer addition: 42 + 5 = 47
+
+print(number + "5")         # String concatenation: "42" + "5" = "425"
